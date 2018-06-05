8856130705dad66f5c3b4faa5fe5e5f49915d65a
leolinf <funlin.py@foxmail.com>
2018-06-05 17:00:23 +0800

    change code

--- blog/app/index/views.py
+++ blog/app/index/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html',locals())
+    return render(request, 'index.html', locals())
+
+
+def new(request):
+    return render(request, 'login.html', locals())
